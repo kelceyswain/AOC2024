1b0a1ad6ad78eb5d3a5fc4d1a18acf888b53192f
Kelcey Swain <kelcey.swain@digital.cabinet-office.gov.uk>
2024-12-12 21:09:11 +0000

    Day 12 part two, part phew more like!

--- 12/aoc_12.py
+++ 12/aoc_12.py
@@ -47,14 +47,26 @@ class Region:
     def __init__(self, plot: Plot, area):
         self._plots = set()
         self.crop = plot.crop
+        self.top = plot.location.y
+        self.bottom = 0
+        self.left = plot.location.x
+        self.right = 0
         self.add_plot(plot, area)
-        self.perimiter_coords = set()
+        self.perimeter_coords = set()
+        self.sides = 0
+
         plot.explored = True
-        self.internal_edges = 0
-        self.external_edges = 0
 
     def add_plot(self, plot, area):
         self._plots.add(plot)
+        if plot.location.x < self.left:
+            self.left = plot.location.x
+        if plot.location.y < self.top:
+            self.top = plot.location.y
+        if plot.location.x > self.right:
+            self.right = plot.location.x
+        if plot.location.y > self.bottom:
+            self.bottom = plot.location.y
         for loc in plot.adjacent_plots():
             if loc in area:
                 adjacent_plot = area.get(loc, None)
@@ -80,14 +92,45 @@ class Region:
     def __len__(self):
         return len(self._plots)
 
-    def perimiter(self):
+    def perimeter(self):
         count = 0
         for plot in self._plots:
             for loc in plot.adjacent_plots():
                 if not self.coord_in_region(loc):
                     count += 1
-                    self.perimiter_coords.add(loc)
+                    self.perimeter_coords.add(loc)
         return count
+
+    def find_sides(self):
+        # Alright, who cares about edges, when we can count corners which is much easier
+        sides = 0
+        for p in self.plots:
+            # convex corners
+            top_left = [p+Coord(-1, 0),  p+Coord(0, -1)]
+            top_right = [p+Coord(0, -1),  p+Coord(1, 0)]
+            bottom_right = [p+Coord(1, 0),  p+Coord(0, 1)]
+            bottom_left = [p+Coord(0, 1), p+Coord(-1, 0)]
+
+            if all([i in self.perimeter_coords for i in top_left]):
+                sides += 1
+            if all([i in self.perimeter_coords for i in top_right]):
+                sides += 1
+            if all([i in self.perimeter_coords for i in bottom_right]):
+                sides += 1
+            if all([i in self.perimeter_coords for i in bottom_left]):
+                sides += 1
+
+            if all([i in self.plots for i in top_left]) and p+Coord(-1, -1) in self.perimeter_coords:
+                sides += 1
+            if all([i in self.plots for i in top_right]) and p+Coord(1, -1) in self.perimeter_coords:
+                sides += 1
+            if all([i in self.plots for i in bottom_right]) and p+Coord(1, 1) in self.perimeter_coords:
+                sides += 1
+            if all([i in self.plots for i in bottom_left]) and p+Coord(-1, 1) in self.perimeter_coords:
+                sides += 1
+
+        self.sides = sides
+        return sides
 
     def __repr__(self):
         return f"Region({self.crop})"
@@ -104,12 +147,19 @@ if __name__ == "__main__":
 
     regions = []
     part_1 = 0
+    part_2 = 0
     for c, a in area.items():
         if a.explored is False:
             regions.append(Region(a, area))
 
     for r in regions:
-        cost = r.perimiter()*len(r)
+        cost = r.perimeter()*len(r)
         part_1 += cost
 
     print(f"Part 1: {part_1}")
+
+    for r in regions:
+        cost = r.find_sides()*len(r)
+        part_2 += cost
+
+    print(f"Part 2: {part_2}")
